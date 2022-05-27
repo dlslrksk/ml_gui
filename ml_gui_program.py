5b50ff061ae3bca44cf53bcb78bb3be942253424
jys <spaceofneed2@gmail.com>
2022-05-27 09:12:45 +0900

    pyqt5_연습

--- ml_gui_program.py
+++ ml_gui_program.py
@@ -2,6 +2,7 @@ from PyQt5.QtWidgets import *
 import sys,pickle
 from PyQt5 import uic, QtWidgets ,QtCore, QtGui
 from data_visualise import data_
+from table_display import DataFrameModel
 
 class UI(QMainWindow):
     def __init__(self):
@@ -11,10 +12,17 @@ class UI(QMainWindow):
         global data, steps
         data = data_()
 
-        self.Browse = self.findChild(QPushButton, "Browse")
-        self.columns = self.findChild(QListWidget, "Browse")
+        self.browse_btn = self.findChild(QPushButton, "browse_btn")
+        self.submit_btn = self.findChild(QPushButton, "submit_btn")
+        self.columns = self.findChild(QListWidget, "column_list")
+        self.table = self.findChild(QTableView, "tableView")
+        self.data_shape = self.findChild(QLabel, "shape")
+        self.label_2 = self.findChild(QLabel, "label_2")
+        self.target_col = self.findChild(QLabel, "target_col")
 
-        self.Browse.clicked.connect(self.getCSV)
+        self.browse_btn.clicked.connect(self.getCSV)
+        self.columns.clicked.connect(self.target)
+        self.submit_btn.clicked.connect(self.set_target)
 
     def filldetails(self, flag = 1):
         if flag == 0 :
@@ -26,9 +34,18 @@ class UI(QMainWindow):
 
         for i, j in enumerate(self.columns_list):
             # print(i, j)
-            stri = f'{j}-------{str(self.df[j].dtype)}'
-            print(stri)
+            stri = f'{j}--{str(self.df[j].dtype)}'
+            # print(stri)
             self.columns.insertItem(i, stri)
+
+        x, y = data.get_shape(self.df)
+        self.data_shape.setText(f'({x},{y})')
+        self.fill_combo_box()
+
+
+    def fill_combo_box(self):
+        x = DataFrameModel(self.df)
+        self.table.setModel(x)
 
 
     def getCSV(self):
@@ -37,6 +54,14 @@ class UI(QMainWindow):
         print(self.filepath)
         if self.filepath != "":
             self.filldetails(0)
+
+    def target(self):
+        self.item = self.columns.currentItem()
+
+
+    def set_target(self):
+        self.target_value = str(self.item.text()).split('--')[0]
+        self.target_col.setText(self.target_value)
 
 
 if __name__ == "__main__":
